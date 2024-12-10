3b4f6915bd38f243740a61b95dea93baaf675101
marco-cst <marco.castillo@unl.edu.ec>
2024-12-10 18:18:22 -0500

    FrontEnd

--- FrontOrdBusq/index.py
+++ FrontOrdBusq/index.py
@@ -1,6 +1,4 @@
 from app import create_app
 app = create_app()
 if __name__ == '__main__':
-    app.secret_key = 'super secret key'
-    app.config['SESSION_TYPE'] = 'filesystem'
     app.run(debug = True, host="0.0.0.0")
